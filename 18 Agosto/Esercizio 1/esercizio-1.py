8b0a3730919e492c51c5c304e47a5b54f21d0183
annasetzu <annasetzu03@gmail.com>
2025-08-18 12:16:17 +0200

    feat: add functions to count words in text and clean the text

--- 18 Agosto/Esercizio 1/esercizio-1.py
+++ 18 Agosto/Esercizio 1/esercizio-1.py
@@ -1,2 +1,13 @@
 def conta_righe(testo):
     return len(testo.splitlines())
+
+def pulisci_testo(testo): 
+    testo = testo.lower()
+    testo = pd.Series([testo]).str.replace(r'[^\w\s]', ' ', regex=True).iloc[0]
+    return testo
+
+def conta_parole(testo):
+    nuovo_testo = pulisci_testo(testo)
+    if not nuovo_testo:
+        return 0
+    return len(nuovo_testo.split())
